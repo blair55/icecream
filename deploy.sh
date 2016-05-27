849405ac6c2db1c0434e16a01fdd1bf61449a4c5
Blair55 <blair.nicholas@googlemail.com>
2016-05-27 16:52:01 +0100

    region.. sigh

--- deploy.sh
+++ deploy.sh
@@ -4,17 +4,18 @@ VERSION=$1
 APP_NAME=icecream
 VERSION_LABEL=$APP_NAME-$VERSION
 DOCKERRUN_FILE=$VERSION_LABEL-Dockerrun.aws.json
-EB_BUCKET=nick-docker-test
-EB_KEY=$APP_NAME/$DOCKERRUN_FILE
+AWS_REGION=eu-west-1
+AWS_BUCKET=nick-docker-test
+AWS_KEY=$APP_NAME/$DOCKERRUN_FILE
 
 # transform template
 sed "s/<TAG>/$VERSION/" < Dockerrun.aws.json.template > $DOCKERRUN_FILE
 
 # copy transformed file to s3
-aws s3 cp $DOCKERRUN_FILE s3://$EB_BUCKET/$EB_KEY --region eu-west-1
+aws --region $AWS_REGION s3 cp $DOCKERRUN_FILE s3://$AWS_BUCKET/$AWS_KEY
 
 # create application version
-aws elasticbeanstalk create-application-version --application-name nick-docker-test --version-label $VERSION_LABEL --source-bundle S3Bucket=$EB_BUCKET,S3Key=$EB_KEY
+aws --region $AWS_REGION elasticbeanstalk create-application-version --application-name nick-docker-test --version-label $VERSION_LABEL --source-bundle S3Bucket=$AWS_BUCKET,S3Key=$AWS_KEY
 
 # deploy new version
-aws elasticbeanstalk update-environment --environment-name ndt-env-3 --version-label $VERSION_LABEL
+aws --region $AWS_REGION elasticbeanstalk update-environment --environment-name ndt-env-3 --version-label $VERSION_LABEL
