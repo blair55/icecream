cae30c89328908f3cb44e690ffec57ecc440c37d
Blair55 <blair.nicholas@googlemail.com>
2016-05-28 10:18:14 +0100

    test diff account

--- deploy.sh
+++ deploy.sh
@@ -5,7 +5,7 @@ VERSION=$1
 APP_NAME=icecream
 VERSION_LABEL=$APP_NAME-$VERSION
 DOCKERRUN_FILE=$VERSION_LABEL-Dockerrun.aws.json
-DOCKERRUN_AWS_BUCKET=elasticbeanstalk-docker-deploys
+DOCKERRUN_AWS_BUCKET=elasticbeanstalk-docker-deploys-2
 DOCKERRUN_AWS_KEY=$APP_NAME/$DOCKERRUN_FILE
 
 # transform template
@@ -18,4 +18,4 @@ aws s3 cp $DOCKERRUN_FILE s3://$DOCKERRUN_AWS_BUCKET/$DOCKERRUN_AWS_KEY
 aws elasticbeanstalk create-application-version --application-name nick-docker-test --version-label $VERSION_LABEL --source-bundle S3Bucket=$DOCKERRUN_AWS_BUCKET,S3Key=$DOCKERRUN_AWS_KEY
 
 # deploy new version
-aws elasticbeanstalk update-environment --environment-name ndt-env-3 --version-label $VERSION_LABEL
+#aws elasticbeanstalk update-environment --environment-name ndt-env-3 --version-label $VERSION_LABEL
