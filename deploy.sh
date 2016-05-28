7001dd174d7a8ee1f8906721b3061878ed5e3f3e
Blair55 <blair.nicholas@googlemail.com>
2016-05-28 09:41:56 +0100

    now actually using eb bucket

--- deploy.sh
+++ deploy.sh
@@ -5,18 +5,17 @@ VERSION=$1
 APP_NAME=icecream
 VERSION_LABEL=$APP_NAME-$VERSION
 DOCKERRUN_FILE=$VERSION_LABEL-Dockerrun.aws.json
-AWS_REGION=eu-west-1
-AWS_BUCKET=nick-docker-test
-AWS_KEY=elasticbeanstalk-docker-deploys/$APP_NAME/$DOCKERRUN_FILE
+DOCKERRUN_AWS_BUCKET=elasticbeanstalk-docker-deploys
+DOCKERRUN_AWS_KEY=$APP_NAME/$DOCKERRUN_FILE
 
 # transform template
 sed "s/<TAG>/$VERSION/" < Dockerrun.aws.json.template > $DOCKERRUN_FILE
 
 # copy transformed file to s3
-aws s3 cp $DOCKERRUN_FILE s3://$AWS_BUCKET/$AWS_KEY
+aws s3 cp $DOCKERRUN_FILE s3://$DOCKERRUN_AWS_BUCKET/$DOCKERRUN_AWS_KEY
 
 # create application version
-aws elasticbeanstalk create-application-version --application-name nick-docker-test --version-label $VERSION_LABEL --source-bundle S3Bucket=$AWS_BUCKET,S3Key=$AWS_KEY
+aws elasticbeanstalk create-application-version --application-name nick-docker-test --version-label $VERSION_LABEL --source-bundle S3Bucket=$DOCKERRUN_AWS_BUCKET,S3Key=$DOCKERRUN_AWS_KEY
 
 # deploy new version
 aws elasticbeanstalk update-environment --environment-name ndt-env-3 --version-label $VERSION_LABEL
